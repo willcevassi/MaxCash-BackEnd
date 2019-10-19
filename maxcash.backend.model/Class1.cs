8cde0c0fede60b43dd2931e506c7361400f21a73
willcevassi <bill.ncc6237@gmail.com>
2019-10-19 15:54:13 -0300

    test2

--- maxcash.backend.model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace maxcash.backend.model
-{
-    public class Class1
-    {
-    }
-}
--- maxcash.backend.repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace maxcash.backend.repository
-{
-    public class Class1
-    {
-    }
-}
